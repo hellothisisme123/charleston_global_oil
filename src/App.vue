a8b594353ed75050c665ed0eb7b838e633f900ff
Luke Lawton <llawton25@student.dimanregional.org>
2022-12-12 20:42:19 -0500

    began the nav bar

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+import navbar from './components/navbar.vue'
+</script>
+
+<template>
+  <navbar></navbar>
+  <div class="bg-[url('./assets/images/bg_oil_rig.jpg')] w-full h-screen"></div>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<style scoped>
+  @font-face {
+    font-family: Courgette;
+    src: url('./assets/fonts/Courgette-Regular.ttf');
+  }
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <div name="navbar" class="h-[10vh] w-full fixed top-0 left-0 bg-gradient-to-b from-[var(--nav-bg-color-start)] to-[var(--nav-bg-color-end)] flex justify-end items-center text-[var(--def-blue)] drop-shadow-[0_0_3px_#000]">
+        <div name="logo image" class="nav_logo_image flex justify-evenly items-center h-full w-[23%] absolute left-0 font-black font-company text-4xl">
+            <img src="../assets/svg/Oil_logo.svg" class="max-h-[80%]" alt="">
+            Charleston Global Oil
+        </div>
+        <div name="nav item" class="w-[10%] h-full outline"></div>
+        <div name="nav item" class="w-[10%] h-full outline"></div>
+        <div name="nav item" class="w-[10%] h-full outline"></div>
+        <div name="nav item" class="w-[10%] h-full outline"></div>
+        <div name="nav item" class="w-[10%] h-full outline"></div>
+        <div name="nav item" class="w-[10%] h-full outline"></div>
+    </div>
+</template>
+
+<style>
+    :root {
+        --nav-bg-color-start: rgb(197, 229, 231, 0.7);
+        --nav-bg-color-end: rgb(197, 229, 231, 0);
+        --logo-font: Courgette;
+        --def-blue: rgb(197, 229, 231);
+    }
+
+    .nav_logo_image {
+        /*-webkit-text-stroke: 0px #fff;*/
+    }
+</style>
